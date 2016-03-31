e22fcac5e772c1c9b58fc5d7abe4d86b00dc2a4f
Lilian Petitpas <lilian.petitpas@outlook.com>
2016-03-31 09:11:53 +0200

    Nouveau Tests
    Un menu qui supporte le passage de la souris sur les éléments + Gestion
    de l'élément choisi

--- src/main.c
+++ src/main.c
@@ -6,13 +6,37 @@ int main (int argc, char** argv)
     SDL_Window* pWindow = NULL;
     SDL_Surface* imageicone = NULL;
     TTF_Font* eraserFont = NULL;
-    SDL_Surface* titreTTF = NULL;
-    SDL_Event event; 
+    SDL_Event event;
     SDL_Surface* surface = NULL;
-    SDL_Rect dstrect;
-    
+    SDL_Surface* titreTTF = NULL;
+    SDL_Rect titreRect;
+    SDL_Color TitreColor = {0, 0, 0, 0};
+    SDL_Surface* JouerTTF = NULL;
+    SDL_Rect JouerRect;
+    SDL_Color JouerColor = {0, 0, 0, 0};
+    int JouerTTFW = 0;
+    int JouerTTFH = 0;
+    SDL_Surface* BestTTF = NULL;
+    SDL_Rect BestRect;
+    SDL_Color BestColor = {0, 0, 0, 0};
+    int BestTTFW = 0;
+    int BestTTFH = 0;
+    SDL_Surface* PasswordTTF = NULL;
+    SDL_Rect PasswordRect;
+    SDL_Color PasswordColor = {0, 0, 0, 0};
+    int PasswordTTFW = 0;
+    int PasswordTTFH = 0;
+    SDL_Surface* OptionTTF = NULL;
+    SDL_Rect OptionRect;
+    SDL_Color OptionColor = {0, 0, 0, 0};
+    int OptionTTFW = 0;
+    int OptionTTFH = 0;
+    SDL_Surface* QuitterTTF = NULL;
+    SDL_Rect QuitterRect;
+    SDL_Color QuitterColor = {0, 0, 0, 0};
+    int QuitterTTFW = 0;
+    int QuitterTTFH = 0;
     short continuer = 1;
-    SDL_Color color={255,0,0,0};
     /* Lecture des parametres envoye par le Shell (Inutile pour le moment) */
     lireParamShell (argc, argv);
 
@@ -47,41 +71,45 @@ int main (int argc, char** argv)
 
     if (!imageicone)
     {
-        fprintf (stderr, "Erreur sur l'image %s: %s\n",IMGICONE, IMG_GetError() );
+        fprintf (stderr, "Erreur sur l'image %s: %s\n", IMGICONE, IMG_GetError() );
     }
     else
     {
-        printf ("Image %s charge\n",IMGICONE);
+        printf ("Image %s charge\n", IMGICONE);
         SDL_SetWindowIcon (pWindow, imageicone);
     }
-    
-    eraserFont=TTF_OpenFont(ERASERFONT, 30);
-    if(!eraserFont) {
-        fprintf (stderr, "Erreur sur la police %s: %s\n",ERASERFONT, TTF_GetError() );
+
+    eraserFont = TTF_OpenFont (ERASERFONT, 30);
+
+    if (!eraserFont)
+    {
+        fprintf (stderr, "Erreur sur la police %s: %s\n", ERASERFONT, TTF_GetError() );
         return EXIT_FAILURE;
     }
     else
     {
-        printf ("Police %s charge\n",ERASERFONT);
+        printf ("Police %s charge\n", ERASERFONT);
     }
-    
+
     /* L'image n'est plus utilise, on peut la supprimer de la memoire */
     SDL_FreeSurface (imageicone);
-    
-    surface = SDL_GetWindowSurface(pWindow);
-    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format,0xff,0xe4,0xc4));
-    
-    if(!(titreTTF=TTF_RenderText_Blended(eraserFont,"Hello World!",color))) {
-        fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
-    } else {
-    dstrect.x = (LARGEUR_FENETRE/2)-(titreTTF->w/2);
-    dstrect.y = (HAUTEUR_FENETRE/2)-(titreTTF->h/2);
-    SDL_BlitSurface(titreTTF,NULL,surface,&dstrect);
-    SDL_FreeSurface(titreTTF);
-}
+    /* On met une couleur de font */
+    surface = SDL_GetWindowSurface (pWindow);
+    SDL_FillRect (surface, NULL, SDL_MapRGB (surface->format, 63, 72, 204) );
 
-    
-    SDL_UpdateWindowSurface(pWindow);
+    /* Le titre */
+    if (! (titreTTF = TTF_RenderText_Blended (eraserFont, "Puzzle MPI1", TitreColor) ) )
+    {
+        fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
+    }
+    else
+    {
+        titreRect.x = (LARGEUR_FENETRE / 2) - (titreTTF->w / 2);
+        titreRect.y = HAUTEUR_FENETRE / 8;
+        SDL_BlitSurface (titreTTF, NULL, surface, &titreRect);
+        SDL_FreeSurface (titreTTF);
+    }
+
     while (continuer)
     {
         SDL_WaitEvent (&event);
@@ -89,11 +117,11 @@ int main (int argc, char** argv)
         switch (event.type)
         {
             case SDL_MOUSEBUTTONDOWN:
-                printf ("Position du click de souris : (%d ; %d)\n", event.button.x, event.button.y);
-                break;
-
-            case SDL_KEYDOWN:
-                printf ("Touche appuye: %s\n", SDL_GetKeyName (event.key.keysym.sym) );
+                printf("%d ",clickMenu (eraserFont, "Jouer", JouerTTFW, JouerTTFH, event, JouerRect));
+                printf("%d ",clickMenu (eraserFont, "Meilleur Score", BestTTFW, BestTTFH, event, BestRect));
+                printf("%d ",clickMenu (eraserFont, "Mot de Passe", PasswordTTFW, PasswordTTFH, event, PasswordRect));
+                printf("%d ",clickMenu (eraserFont, "Options", OptionTTFW, OptionTTFH, event, OptionRect));
+                printf("%d\n",clickMenu (eraserFont, "Quitter", QuitterTTFW, QuitterTTFH, event, QuitterRect));
                 break;
 
             case SDL_QUIT:
@@ -101,9 +129,80 @@ int main (int argc, char** argv)
                 continuer = 0;
                 break;
 
+            case SDL_MOUSEMOTION:
+                JouerColor.r = hoverMenu (eraserFont, "Jouer", JouerTTFW, JouerTTFH, event, JouerRect);
+                BestColor.r = hoverMenu (eraserFont, "Meilleur Score", BestTTFW, BestTTFH, event, BestRect);
+                PasswordColor.r = hoverMenu (eraserFont, "Mot de Passe", PasswordTTFW, PasswordTTFH, event, PasswordRect);
+                OptionColor.r = hoverMenu (eraserFont, "Options", OptionTTFW, OptionTTFH, event, OptionRect);
+                QuitterColor.r = hoverMenu (eraserFont, "Quitter", QuitterTTFW, QuitterTTFH, event, QuitterRect);
             default:
                 break;
         }
+
+        /* On Affiche tout ce qui est a afficher dynamiquement comme le texte */
+
+        /* Jouer */
+        if (! (JouerTTF = TTF_RenderText_Solid (eraserFont, "Jouer", JouerColor) ) )
+        {
+            fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
+        }
+        else
+        {
+            JouerRect.x = (LARGEUR_FENETRE / 2) - (JouerTTF->w / 2);
+            JouerRect.y = (3 * HAUTEUR_FENETRE) / 8;
+            SDL_BlitSurface (JouerTTF, NULL, surface, &JouerRect);
+            SDL_FreeSurface (JouerTTF);
+        }
+
+        if (! (BestTTF = TTF_RenderText_Solid (eraserFont, "Meilleur Score", BestColor) ) )
+        {
+            fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
+        }
+        else
+        {
+            BestRect.x = (LARGEUR_FENETRE / 2) - (BestTTF->w / 2);
+            BestRect.y = (4 * HAUTEUR_FENETRE) / 8;
+            SDL_BlitSurface (BestTTF, NULL, surface, &BestRect);
+            SDL_FreeSurface (BestTTF);
+        }
+
+        if (! (PasswordTTF = TTF_RenderText_Solid (eraserFont, "Mot de passe", PasswordColor) ) )
+        {
+            fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
+        }
+        else
+        {
+            PasswordRect.x = (LARGEUR_FENETRE / 2) - (PasswordTTF->w / 2);
+            PasswordRect.y = (5 * HAUTEUR_FENETRE) / 8;
+            SDL_BlitSurface (PasswordTTF, NULL, surface, &PasswordRect);
+            SDL_FreeSurface (PasswordTTF);
+        }
+
+        if (! (OptionTTF = TTF_RenderText_Solid (eraserFont, "Options", OptionColor) ) )
+        {
+            fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
+        }
+        else
+        {
+            OptionRect.x = (LARGEUR_FENETRE / 2) - (OptionTTF->w / 2);
+            OptionRect.y = (6 * HAUTEUR_FENETRE) / 8;
+            SDL_BlitSurface (OptionTTF, NULL, surface, &OptionRect);
+            SDL_FreeSurface (OptionTTF);
+        }
+
+        if (! (QuitterTTF = TTF_RenderText_Solid (eraserFont, "Quitter", QuitterColor) ) )
+        {
+            fprintf (stderr, "Erreur lors de l'ecriture: %s\n", TTF_GetError() );
+        }
+        else
+        {
+            QuitterRect.x = (LARGEUR_FENETRE / 2) - (QuitterTTF->w / 2);
+            QuitterRect.y = (7 * HAUTEUR_FENETRE) / 8;
+            SDL_BlitSurface (QuitterTTF, NULL, surface, &QuitterRect);
+            SDL_FreeSurface (QuitterTTF);
+        }
+
+        SDL_UpdateWindowSurface (pWindow);
     }
 
     SDL_DestroyWindow (pWindow);
@@ -121,4 +220,46 @@ void lireParamShell (int argc, char** argv)
     {
         printf ("Parametre %d envoye: %s \n", i, argv[i]);
     }
+}
+
+short hoverMenu (TTF_Font* Font, char* String, int TTFW, int TTFH, SDL_Event event, SDL_Rect Rect)
+{
+    TTF_SizeText (Font, String, &TTFW, &TTFH);
+
+    if (event.motion.x > Rect.x)
+    {
+        if (event.motion.y > Rect.y)
+        {
+            if (event.motion.x < (Rect.x + TTFW) )
+            {
+                if (event.motion.y < (Rect.y + TTFH) )
+                {
+                    return 255;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+short clickMenu (TTF_Font* Font, char* String, int TTFW, int TTFH, SDL_Event event, SDL_Rect Rect)
+{
+    TTF_SizeText (Font, String, &TTFW, &TTFH);
+
+    if (event.button.x > Rect.x)
+    {
+        if (event.button.y > Rect.y)
+        {
+            if (event.button.x < (Rect.x + TTFW) )
+            {
+                if (event.button.y < (Rect.y + TTFH) )
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    return 0;
 }
--- src/main.h
+++ src/main.h
@@ -7,5 +7,7 @@
 #define HAUTEUR_FENETRE 480
 
 void lireParamShell (int argc, char** argv);
+short hoverMenu(TTF_Font* Font,char* String,int TTFW, int TTFH,SDL_Event event,SDL_Rect Rect) ;
+short clickMenu(TTF_Font* Font,char* String,int TTFW, int TTFH,SDL_Event event,SDL_Rect Rect) ;
 
 #endif
